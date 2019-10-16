35a3a8520d73f619d536cf000a5ef6945e5f36eb
Rafaela Fernandes <rafaelafernandes22@hotmail.com>
2019-10-16 09:35:38 -0300

    inserção do modal e scrool rolagem

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
- $(document).ready(function() {
+ $(document).ready(function () {
 
-            // Script de animaca do menu quando mobile
-            $(".navbar-toggler").click(function() {
+     // Script de animaca do menu quando mobile
+     $(".navbar-toggler").click(function () {
 
-                $("#nav-inema").toggle("slide", {
-                    direction: "right"
-                }, 700)
-            })
+         $("#nav-inema").toggle("slide", {
+             direction: "right"
+         }, 700)
+     })
 
-            $("#nav-inema a").click(function() {
-                $("#nav-inema").hide('fade')
+     $("#nav-inema a").click(function () {
+         $("#nav-inema").hide('fade')
 
-            })
-        })
+     })
+     
+     
+     // scroll para rolar ate alvo
+
+     $(".scroll").click(function (event) {
+         event.preventDefault();
+         $('html,body').animate({
+             scrollTop: $(this.hash).offset().top
+         }, 600);
+       
+     });
+ })
